fbd88ef22de2acf870035ffaf9baf532e177f43c
Your Name <you@example.com>
2025-12-09 11:51:03 +0530

    05-theme-styling

--- features/auth/components/login-form.tsx
+++ features/auth/components/login-form.tsx
@@ -82,8 +82,9 @@ export function LoginForm() {
                                        className="w-full"
                                        type="button"
                                        disabled={isPending}
-                                    >
-                                        Continue with Github
+                                    > 
+                                        <Image alt="GitHub" src="/logos/github.svg" width={20} height={20} />
+                                        Continue with GitHub
 
                                     </Button>
                                     <Button 
@@ -92,6 +93,7 @@ export function LoginForm() {
                                        type="button"
                                        disabled={isPending}
                                     >
+                                         <Image alt="Google" src="/logos/google.svg" width={20} height={20} />
                                         Continue with Google
                                     </Button>
                                 </div>
