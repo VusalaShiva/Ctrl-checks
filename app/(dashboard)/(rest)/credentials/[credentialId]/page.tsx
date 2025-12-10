86dca2453edf0b7f45c443c3bd617cff6665bed8
Your Name <you@example.com>
2025-12-10 18:13:58 +0530

    09: sidebar layout

---
+++ app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,22 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+
+interface PageProps {
+    params: Promise < {
+        credentialId: string;
+    }>
+};
+
+
+// https://localhost:3000/credentials/123
+
+const Page = async ({ params } : PageProps) => { 
+    await requireAuth();
+    const { credentialId } = await params;
+    
+    return (
+        <p>Credential id : {credentialId}</p>
+    );
+};
+
+export default Page;
