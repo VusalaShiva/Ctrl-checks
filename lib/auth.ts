8d86f18b280b48ba1f5f50ec3a8dc8d4323627de
Your Name <you@example.com>
2025-12-11 09:47:42 +0530

    10: payment setup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
-
+import {checkout, polar, portal} from "@polar-sh/better-auth"
 import {prismaAdapter} from "better-auth/adapters/prisma";
-
+import { polarClient} from "./polar";
 import prisma from "@/lib/db";
 
 export const auth = betterAuth({
@@ -11,7 +11,27 @@ export const auth = betterAuth({
   emailAndPassword :{
     enabled : true,
     autoSignIn : true,
-  }
+  },
+  plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "0e55472f-7bd1-4d0f-ab09-06489e70f370",
+                            slug: "pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/CtrlChecks-Pro
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal()
+            ],
+        }),
+       
+    ]
 });
 
 
